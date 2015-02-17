554cd1451f01adf1b99dfbce4c2049eef88e1760
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-17 17:32:33 -0500

    adds some formattin in user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
 
-has_secure_password
+  has_secure_password
 
-def self.students
-  User.where user_type: "students"
-end
+  def self.students
+    User.where user_type: "students"
+  end
 
-def self.instructors
-  User.where user_type: "instructor"
-end
+  def self.instructors
+    User.where user_type: "instructor"
+  end
 
-def self.admissions
-  User.where user_type: "admissions"
-end
+  def self.admissions
+    User.where user_type: "admissions"
+  end
 
-end
+end#user
