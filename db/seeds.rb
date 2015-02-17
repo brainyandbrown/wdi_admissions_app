e9b301e80b41b336615c3e4cf879d7ca482fe2c7
Angelina Bethoney <ambethoney@yahoo.com>
2015-02-17 13:56:23 -0500

    pushing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+INSERT INTO students(:f_name, :l_name, :sex, :born_on, :email, :phone, :password_digest) VALUES{
+  ["Angelina", "Bethoney", "female", "04/13/1989", "ambethoney@yahoo.com", "774-291-0958", "password1"],
+  ["Dana", "DeFilipo", "female", "04/14/1991", "dana.dflip@gmail.com", "516-987-8427", "dflip"],
+  ["Sade", "Stevens", "female", "11/10/1991", "sadeshakur@gmail.com", "516-043-7720", "cheese"],
+  ["Dennis", "Liaw", "male", "05/16/1987", "dennisliaw@gmail.com", "678-455-9920", "dennis"]
+}
