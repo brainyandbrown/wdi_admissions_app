cabfc026a087b78854ff050e78c1b10f28c2cdfe
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-17 15:15:29 -0500

    adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#("f_name", "l_name", "sex", "email", "phone", "password_digest", "created_at", "updated_at")
 Student.create([
-  { f_name: "Angelina", l_name: "Bethoney", sex: "female", born_on: "04/13/1989", email: "ambethoney@yahoo.com", phone: "774-291-0958", password_digest: },
-  { f_name: "Dana", l_name: "DeFilipo", sex: "female", born_on: "04/14/1991", email: "dana.dflip@gmail.com", phone: "516-987-8427", password_digest: },
-  { f_name: "Sade", l_name: "Stevens", sex: "female", born_on: "11/10/1991", email: "sadeshakur@gmail.com", phone: "516-043-7720", password_digest: },
-   { f_name: "Dennis", l_name: "Liaw", sex: "male", born_on: "05/16/1987", email: "dennisliaw@gmail.com", phone: "678-455-9920", password_digest: }
+  { f_name: "Angelina", l_name: "Bethoney", sex: "female", born_on: "April 13 1989", email: "ambethoney@yahoo.com", phone: "774-291-0958", password: "password" },
+  { f_name: "Dana",     l_name: "DeFilipo", sex: "female", born_on: "April 14 1991", email: "dana.dflip@gmail.com", phone: "516-987-8427", password: "password" },
+  { f_name: "Sade",     l_name: "Stevens",  sex: "female", born_on: "November 10 1991", email: "sadeshakur@gmail.com", phone: "516-043-7720", password: "password" },
+  { f_name: "Dennis",   l_name: "Liaw",     sex: "male",   born_on: "May 16 1987", email: "dennisliaw@gmail.com", phone: "678-455-9920", password: "password" }
 ])
 
 
-
-
+# t.string :f_name, null: false
+# t.string :l_name, null: false
+# t.string :sex, null: false
+# t.date   :born_on, null: false
+# t.string :email, null: false, unique: true
+# t.string :phone, null: false
+# t.string :password_digest, null: false
+# t.timestamps
