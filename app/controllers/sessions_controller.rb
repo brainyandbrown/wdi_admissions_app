118db7fe7a51fd58a42bcc373aef7054fbecebc5
Sade Stevens <sadestevens.hc@gmail.com>
2015-02-17 15:18:16 -0500

    something

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+def create
+
+email    = params[:email]
+password = params[:password]
+student = Student.find_by(email: email)
+
+if student && student.authenticate(pass)
+  session[:student_id] = student.student_id
+  redirect_to '/home'
+else
+  redirect_to '/login'
+end
+
+end
