bcb0a91341ca3ff015b642a248d7f17bfcb06a7c
Dana DeFilippo <dana.dflip@gmail.com>
2015-02-17 19:13:40 -0500

    updates stuff

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,20 @@ def home
 
 end
 
+
+# paperclip methods
+def create
+  @student = Student.create( student_params )
+end
+
+private
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+def student_params
+  params.require(:students).permit(:portfolio)
+end
+
+
 end
