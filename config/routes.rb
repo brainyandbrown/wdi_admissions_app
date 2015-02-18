ae8dad6f23bbac4218aa6cf2b03ea3e63b2e36db
Dana DeFilippo <dana.dflip@gmail.com>
2015-02-18 11:22:30 -0500

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,9 @@ Rails.application.routes.draw do
 
   #get "/home" => 'students#home'
 
-<<<<<<< HEAD
-=======
-  #this will change depending on user type
-  get '/home' => "users#show"
-
   resources :students
   resources :instructors
   resources :admissions
 
->>>>>>> 7ec7e5d1eb27871f1a3e4fb155df9b1df110641d
+
 end
