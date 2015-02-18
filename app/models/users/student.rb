c02d369e5efdc5c77a5d8d1a55372d34ef7e2c8a
Dana DeFilippo <dana.dflip@gmail.com>
2015-02-18 10:46:06 -0500

    modified student.rb

--- app/models/users/student.rb
+++ app/models/users/student.rb
@@ -1,7 +1,7 @@
 class Student < User
 
 # paperclip:
-  has_attached_file :portfolio, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :portfolio, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :portfolio, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :portfolio, :content_type => /\Aimage\/.*\Z/
 
 end
