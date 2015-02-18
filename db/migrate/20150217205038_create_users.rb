45dd95e220664e05cb37497fc9c94cd675a933f3
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-18 13:54:35 -0500

    wtf db

--- db/migrate/20150217205038_create_users.rb
+++ db/migrate/20150217205038_create_users.rb
@@ -6,8 +6,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :l_name
       t.string :sex
       t.date   :born_on
-      t.string :email,           null: false, unique: true
       t.string :phone
+      t.string :email, null: false, unique: true
       t.string :password_digest, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150218154255) do
   enable_extension "plpgsql"
 
   create_table "prospects", force: :cascade do |t|
-    t.integer "students_id"
+    t.integer "students_id",                          null: false
     t.boolean "phone_screen",         default: false
     t.boolean "instructor_interview", default: false
     t.boolean "offered",              default: false
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 20150218154255) do
 
   create_table "users", force: :cascade do |t|
     t.string   "type"
-    t.string   "f_name",          null: false
-    t.string   "l_name",          null: false
-    t.string   "sex",             null: false
-    t.date     "born_on",         null: false
+    t.string   "f_name"
+    t.string   "l_name"
+    t.string   "sex"
+    t.date     "born_on"
+    t.string   "phone"
     t.string   "email",           null: false
-    t.string   "phone",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
